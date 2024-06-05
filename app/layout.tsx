a281c46671cbdff044f9ba0d0da9c35704e0ef7b
super-SH <dev.swam.htet@gmail.com>
2024-06-06 01:25:27 +0630

    feat: add navigation and logo component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Navigation from '@/app/components/Navigation';
+import Logo from '@/app/components/Logo';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
