47127dd4a92a11107f9048d83d7be4438f8cd718
super-SH <dev.swam.htet@gmail.com>
2024-06-07 00:53:25 +0630

    feat: add metadata and favicon

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Guest Area",
+};
 
 function AccountPage() {
   return <div>AccountPage</div>;
--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Cabins",
+};
 
 function CabinsPage() {
   return <div>CabinsPage</div>;
