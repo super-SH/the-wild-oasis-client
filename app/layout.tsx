c922b8e3d86a66ea8fedf0899d234d1218ac4de2
super-SH <dev.swam.htet@gmail.com>
2024-06-07 00:36:29 +0630

    feat: add colors into tailwind config

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Navigation from '@/app/_components/Navigation';
 import Logo from '@/app/_components/Logo';
+import '@/app/_styles/globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
+      <body className='min-h-screen bg-primary-950 text-primary-100'>
         <header>
           <Logo />
           <Navigation />
