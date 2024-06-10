8f3baaaf4d2ece3450ca5b507e7fcf0995559d75
super-SH <dev.swam.htet@gmail.com>
2024-06-11 01:00:18 +0630

    feat: add single cabin details page

--- app/_lib/data-service.ts
+++ app/_lib/data-service.ts
@@ -1,5 +1,22 @@
 import { supabase } from "./supabase";
 
+/////////////
+// GET
+
+export async function getCabin(id: number) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+  }
+
+  return data;
+}
+
 export async function getCountries() {
   try {
     const res = await fetch(
